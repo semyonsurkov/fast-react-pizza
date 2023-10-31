977ee8961cce27dbfc4db8a27589e76f21cf729c
semyonsurkov <semyonsurkov@outlook.com>
2023-11-01 01:54:08 +0300

    Add new components

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,20 +1,22 @@
-import { Outlet, useNavigate } from 'react-router-dom';
-import CartOverview from '../features/cart/CartOverview';
-import Header from './Header';
-import Loader from './Loader';
+import { Outlet, useNavigate } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
+import Header from "./Header";
+import Loader from "./Loader";
 
 function AppLayout() {
   const navigation = useNavigate();
-  const isLoading = navigation.state === 'loading';
+  const isLoading = navigation.state === "loading";
 
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto] ">
       {isLoading && <Loader />}
       <Header />
 
-      <main>
-        <Outlet />
-      </main>
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
     </div>
