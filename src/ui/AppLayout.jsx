533ab9a65711d5e5a05aa44f5b9f0fbbb6c18c08
semyonsurkov <semyonsurkov@outlook.com>
2023-10-31 13:46:30 +0300

    Add basic structure for the project

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,24 @@
+import { Outlet, useNavigate } from 'react-router-dom';
+import CartOverview from '../features/cart/CartOverview';
+import Header from './Header';
+import Loader from './Loader';
+
+function AppLayout() {
+  const navigation = useNavigate();
+  const isLoading = navigation.state === 'loading';
+
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
