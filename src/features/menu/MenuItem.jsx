e136e1ab0e7308db2e93b5087d8d12e633933726
semyonsurkov <semyonsurkov@outlook.com>
2023-11-02 14:49:25 +0300

    Add CartOverview functions

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,20 +1,35 @@
-import { formatCurrency } from "../../utilities/helpers";
-import Button from "../../ui/Button";
+import { useDispatch } from 'react-redux';
+import { formatCurrency } from '../../utilities/helpers';
+import Button from '../../ui/Button';
+import { addItem } from '../cart/cartSlice';
 
 function MenuItem({ pizza }) {
+  const dispatch = useDispatch();
+
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+  }
 
   return (
     <li className="flex gap-4 py-2">
       <img
         src={imageUrl}
         alt={name}
-        className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
+        className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ''}`}
       />
       <div className="flex grow flex-col pt-0.5">
         <p className="font-medium">{name}</p>
         <p className="text-sm capitalize italic text-stone-500">
-          {ingredients.join(", ")}
+          {ingredients.join(', ')}
         </p>
         <div className="f mt-auto flex items-center justify-between">
           {!soldOut ? (
@@ -25,7 +40,11 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          <Button type="small">Add to cart</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
